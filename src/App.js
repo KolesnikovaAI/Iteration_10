e83f612b46dd9d0f4db0af132bb2203e5d5a3d16
Anastasiya Kolesnikova <KolesnikovaAI93@yandex.ru>
2022-08-01 17:13:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import "./App.css";
-import React, { Component } from "react";
+import Profile from "./Profile";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "",
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    //функция,получающая значение из инпута
-    this.setState({ value: event.target.value }); //меняем состояние value, получая значение из инпута
-  }
-
-  getUser(login) {
-    //функция, возвращающая промис с объектом пользователя
-    return fetch(`https://api.github.com/users/${login}`).then((response) =>
-      response.json()
-    );
-  }
-
-  async handleSubmit(event) {
-    //async -функция асинхронная
-    event.preventDefault(); //отменяет действия по умолчанию
-    let user = await this.getUser(this.state.value); // ждет,когда выполниться промис и придет результат для значения this.state.value
-    this.setState({ id: user.id }); //изменяем состояние присваивая значение из user
-    this.setState({ imgSrc: user.avatar_url });
-  }
-  render() {
-    return (
-      <div>
-        <h1>Поиск информации о GitHub пользователе</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="Введите GIT"
-            onChange={this.handleChange}
-          ></input>
-          <input type="submit" value="GO!"></input>
-        </form>
-        <div>{this.state.id}</div>
-        <img src={this.state.imgSrc}></img>
-      </div>
-    );
-  }
-}
-
+const App = (props) => {
+  return (
+    // <BrowserRouter>
+    //   <div>
+    //     <Route path = '/profile' component = {Profile}/>
+    //   </div>
+    // </BrowserRouter>
+    <Profile />
+  );
+};
 export default App;
